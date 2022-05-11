1fa85250efc9d90655bc02fac0c132d0af28254f
李密 <987216152@qq.com>
2022-05-11 16:04:03 +0800

    文本工具添加

---
+++ LMFrame/Assets/Scripts/TextUtil/TextRoll.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextRoll : Text
+{
+    private long m_Cur;
+    private long m_Target;
+    private float m_Offset;
+    private bool m_IsBegin = false;
+    private bool m_IsAdd;
+    private float CurCount;
+    //传入一个当前数，一个目标数，可以实现从当前数变换到目标数的效果
+    public void ChangeText(long cur, long target)
+    {
+        m_Cur = cur;
+        m_Target = target;
+        m_Offset = m_Target - m_Cur;
+        m_IsBegin = true;
+        CurCount = 0;
+
+        if (m_Target < m_Cur)
+            m_IsAdd = false;
+        else if (m_Target > m_Cur)
+            m_IsAdd = true;
+    }
+
+    void Update()
+    {
+        Debug.Log("??");
+        if (!m_IsBegin)
+            return;
+
+        CurCount += m_Offset * Time.deltaTime * 2;
+        if (Mathf.Abs(CurCount) > 1)
+        {
+            m_Cur += Mathf.FloorToInt(CurCount);
+            CurCount -= Mathf.FloorToInt(CurCount);
+        }
+
+        if ((m_IsAdd && m_Cur > m_Target)
+            || (!m_IsAdd && m_Cur < m_Target))
+        {
+            m_IsBegin = false;
+            m_Cur = m_Target;
+        }
+        Debug.Log(CurCount + "__" + m_Cur);
+        text = m_Cur.ToString();
+    }
+}
